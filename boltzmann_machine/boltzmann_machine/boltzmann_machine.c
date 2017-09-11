a3393dd2c0a290dc7f15bea965132d231e20e842
user <user@localhost>
2017-09-11 17:30:18 +0900

    方向性を決める

---
+++ boltzmann_machine/boltzmann_machine/boltzmann_machine.c
@@ -0,0 +1,79 @@
+﻿#include "boltzmann_machine.h"
+#include <stdlib.h>
+#include <math.h>
+
+BM *bm_new(static int node_num) {
+	BM *bm = (BM *)malloc(sizeof(BM));
+
+	bm->nodeNum = node_num;
+	bm->node = (double *)calloc(node_num, sizeof(double));
+	bm->bias = (double *)calloc(node_num, sizeof(double));
+	bm->weight = (double **)calloc(node_num, sizeof(double *));
+	bm->adjacency_matrix = (int **)calloc(node_num, sizeof(int *));
+
+	for (int i = 0; i < node_num; i++) {
+		bm->weight[i] = (double *)calloc(node_num, sizeof(double));
+		bm->adjacency_matrix[i] = (int *)calloc(node_num, sizeof(int));
+	}
+}
+
+void *bm_free(BM *bm) {
+	free(bm->node);
+	free(bm->bias);
+
+	for (int i = 0; i < bm->nodeNum; i++) {
+		free(bm->weight[i]);
+		free(bm->adjacency_matrix[i]);
+	}
+
+	free(bm);
+
+	return NULL;
+}
+
+void bm_init(BM *bm) {
+	// TODO: パラメータの初期化, 隣接行列の初期化をせよ
+}
+
+double bm_conditional_probablity(BM *bm, int node_index, double value) {
+	double energy = bm->bias[node_index];
+
+	// 行探索
+	for (int i = 0; i < bm->nodeNum; i++) {
+		if (i == node_index) continue;
+		if (bm->adjacency_matrix[i][node_index] != 1) continue;  // 隣接行列で0の要素は計算しない
+
+		energy += bm->weight[i][node_index] * bm->node[node_index];
+	}
+
+	// 列探索
+	for (int j = 0; j < bm->nodeNum; j++) {
+		if (j == node_index) continue;
+		if (bm->adjacency_matrix[node_index][j] != 1) continue;  // 隣接行列で0の要素は計算しない
+
+		energy += bm->weight[node_index][j] * bm->node[node_index];
+	}
+
+	return exp(value * energy) / (2 * cosh(energy));
+}
+
+
+double bm_gibbs_sampling(BM *bm, int node_index) {
+	double u = bm_uniform();  // 一様乱数
+	double prob = bm_conditional_probablity(bm, node_index, -1.0);
+
+	double value = u < prob ? -1.0 : 1.0;
+
+	return value;
+}
+
+double bm_uniform() {
+	return ((double)rand() + 1.0) / ((double)RAND_MAX + 2.0);
+}
+
+double bm_update_by_gibbs_sampling(BM *bm, int node_index) {
+	double value = bm_gibbs_sampling(bm, node_index);
+	bm->node[node_index] = value;
+
+	return value;
+}
---
+++ boltzmann_machine/boltzmann_machine/boltzmann_machine.h
@@ -0,0 +1,75 @@
+﻿#pragma once
+#include <stdlib.h>
+
+typedef struct {
+	int nodeNum;  // ノード数
+	double *node;  // ノード変数
+	double *bias;  //バイアス
+	double **weight;  // ウェイト
+	int ** adjacency_matrix;  // 隣接行列(全結合BMだと別になくてもいいけど)
+} BM;
+
+typedef struct {
+	int dataSize;
+	int nodeNum;
+	int dataset;  // dataset[DATA_INDEX][NODE_INDEX]
+} BM_DATASET;
+
+/**
+* @fn
+* ボルツマンマシン構造体確保
+* @param (node_num) ノード数
+* @return ボルツマンマシン構造体のポインタ
+*/
+BM *bm_new(static int node_num);
+
+/**
+* @fn
+* ボルツマンマシン構造体解放, 二重解放予防にNULL返す
+* @param (node_num) ボルツマンマシン構造体のポインタ
+* @return NULL
+*/
+void *bm_free(BM *bm);
+
+/**
+* @fn
+* ボルツマンマシン構造体のパラメータ初期化
+* @param (bm) ボルツマンマシン構造体のポインタ
+*/
+void bm_init(BM *bm);
+
+/**
+* @fn
+* ボルツマンマシンの条件付き確率
+* @param (bm) ボルツマンマシン構造体のポインタ
+* @param (node_index) ノード番号
+* @param (value) 値
+* @return 確率値
+*/
+double bm_conditional_probablity(BM *bm, int node_index, double value);
+
+/**
+* @fn
+* ボルツマンマシンのギブスサンプリング(x_i \in {-1, +1})
+* @param (bm) ボルツマンマシン構造体のポインタ
+* @param (node_index) ノード番号
+* @return サンプル値
+*/
+double bm_gibbs_sampling(BM *bm, int node_index);
+
+/**
+* @fn
+* [0, 1]の乱数(一応インターフェースのつもり, 適当に実装してもらいたし。)
+* @return 乱数値
+*/
+double bm_uniform();
+
+/**
+* @fn
+* ギブスサンプリングによるボルツマンマシンの更新
+* @param (bm) ボルツマンマシン構造体のポインタ
+* @param (node_index) ノード番号
+* @return サンプル値
+*/
+double bm_update_by_gibbs_sampling(BM *bm, int node_index);
+
